7061492ad11dc68517abcf34e787073d11c4b510
Pavel Surkov <surkovv.pavel@gmail.com>
2022-10-20 21:37:12 +0700

    adds styles for opened mobile menu

--- public/js/application.js
+++ public/js/application.js
@@ -7,9 +7,11 @@ document.addEventListener(`DOMContentLoaded`, () => {});
 const menuBtn = document.querySelector('#m-menu-toggle');
 const menuElement = document.querySelector(menuBtn.dataset.target);
 const blocker = document.querySelector('#blocker');
+const mobileHeader = document.querySelector('#mobile-header');
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('navbar-toggler-close');
   menuElement.classList.toggle('m-menu-opened');
   blocker.classList.toggle('blocker-opened');
+  mobileHeader.classList.toggle('aside-primary-light');
 });
