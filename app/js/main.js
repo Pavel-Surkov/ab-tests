62e6d8acc274af6e4429d60a6220c796b925b0a4
Pavel Surkov <surkovv.pavel@gmail.com>
2022-10-20 17:21:36 +0700

    adds js for mobile menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,13 @@
 
 document.addEventListener(`DOMContentLoaded`, () => {});
 
-// JQUERY
+// Mobile menu toggle
+const menuBtn = document.querySelector('#m-menu-toggle');
+const menuElement = document.querySelector(menuBtn.dataset.target);
+const blocker = document.querySelector('#blocker');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('navbar-toggler-close');
+  menuElement.classList.toggle('m-menu-opened');
+  blocker.classList.toggle('blocker-opened');
+});
