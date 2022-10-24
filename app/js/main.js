822a9bce81bd4549245ed0d21b7f83681cad861c
Pavel Surkov <surkovv.pavel@gmail.com>
2022-10-24 21:14:41 +0700

    connects progress bars to percentage values in cognitive score

--- app/js/main.js
+++ app/js/main.js
@@ -3,14 +3,30 @@
 document.addEventListener(`DOMContentLoaded`, () => {});
 
 // Mobile menu toggle
-const menuBtn = document.querySelector('#m-menu-toggle');
-const menuElement = document.querySelector(menuBtn.dataset.target);
-const blocker = document.querySelector('#blocker');
-const mobileHeader = document.querySelector('#mobile-header');
+(function toggleMobileMenu() {
+  const menuBtn = document.querySelector('#m-menu-toggle');
+  const menuElement = document.querySelector(menuBtn.dataset.target);
+  const blocker = document.querySelector('#blocker');
+  const mobileHeader = document.querySelector('#mobile-header');
 
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('navbar-toggler-close');
-  menuElement.classList.toggle('m-menu-opened');
-  blocker.classList.toggle('blocker-opened');
-  mobileHeader.classList.toggle('aside-primary-light');
-});
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('navbar-toggler-close');
+    menuElement.classList.toggle('m-menu-opened');
+    blocker.classList.toggle('blocker-opened');
+    mobileHeader.classList.toggle('aside-primary-light');
+  });
+})();
+
+// Function for scores to connect svg to percentage
+(function calculateScoreRound() {
+  const scoreCountElements = document.querySelectorAll('.progress-res');
+
+  scoreCountElements.forEach((el) => {
+    const svgBoxElement = el.nextElementSibling;
+    const circleElement = svgBoxElement.querySelector('.progress-circle');
+
+    const scoreInPercent = parseInt(el.innerHTML);
+
+    circleElement.style.strokeDasharray = `${scoreInPercent}, 100`;
+  });
+})();
